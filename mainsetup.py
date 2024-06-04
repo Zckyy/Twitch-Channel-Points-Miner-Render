4a2c5848ee926519b77c201545018bd7613163b2
Zack Polc <zack.polc@gmail.com>
2024-06-04 15:47:55 +0100

    login

--- mainsetup.py
+++ mainsetup.py
@@ -19,7 +19,7 @@ keep_alive()
 
 twitch_miner = TwitchChannelPointsMiner(
     username="zack280599",
-    password="",           # If no password will be provided, the script will ask interactively
+    password="Tihobuf3",           # If no password will be provided, the script will ask interactively
     claim_drops_startup=False,                  # If you want to auto claim all drops from Twitch inventory on the startup
     priority=[                                  # Custom priority in this case for example:
         Priority.STREAK,                        # - We want first of all to catch all watch streak from all streamers
